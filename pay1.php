1d0d7093739e8cfde40d0823726da71c55ee416d
zerocoolstack <51968750+zerocoolstack@users.noreply.github.com>
2019-06-18 19:40:45 -0300

    Update pay1.php
    No se elimina toda la sesión

--- pay1.php
+++ pay1.php
@@ -2,6 +2,7 @@
 session_start();
 $payment = 'Completed';
 if($payment === 'Completed'){
-    session_destroy();
+    $_SESSION['datos_form']['__step__'] = 1;
+    $_SESSION['datos_form']['radio'] = 1;
 }
 ?>
